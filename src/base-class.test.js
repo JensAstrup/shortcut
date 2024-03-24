2b7f0c3f5d9610197870fa040015cf731c0e8d0d
Jens Astrup <jensaiden@gmail.com>
2024-03-24 19:52:28 -0400

    Update paths to not collide with 3rd party packages. Get tests working

---
+++ src/base-class.test.js
@@ -0,0 +1,71 @@
+import axios from 'axios'
+import process from 'process'
+import ShortcutResource from './base-class'
+
+
+jest.mock('axios', () => ({
+    get: jest.fn(),
+    post: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn()
+}))
+
+
+describe('ShortcutResource', () => {
+    const mockHeaders = {
+        'Content-Type': 'application/json',
+        'Shortcut-Token': 'token'
+    }
+    process.env.SHORTCUT_API_KEY = 'token'
+
+    describe('constructor and Proxy setup', () => {
+        it('initializes with provided object', () => {
+            const init = {name: 'Test'}
+            const resource = new ShortcutResource(init)
+            expect(resource.name).toBe('Test')
+        })
+    })
+
+    describe('save method', () => {
+        it('calls update if id exists', async () => {
+            const resource = new ShortcutResource({id: 123})
+            resource.name = 'Updated Name'
+            axios.put.mockResolvedValue({data: {snake_name: 'Updated Name'}})
+
+            await resource.save()
+
+            expect(axios.put).toHaveBeenCalledWith(expect.any(String), expect.any(Object), {headers: mockHeaders})
+            expect(resource.name).toBe('Updated Name')
+        })
+
+        it('calls create if id does not exist', async () => {
+            const resource = new ShortcutResource()
+            resource.name = 'New Name'
+            axios.post.mockResolvedValue({data: {id: 123, snake_name: 'New Name'}})
+
+            await resource.save()
+
+            expect(axios.post).toHaveBeenCalledWith(expect.any(String), expect.any(Object), {headers: mockHeaders})
+            expect(resource.id).toBe(123)
+            expect(resource.name).toBe('New Name')
+        })
+    })
+
+    describe('delete method', () => {
+        it('sends a delete request for the resource', async () => {
+            const resource = new ShortcutResource({id: 123})
+            axios.delete.mockResolvedValue({})
+
+            await resource.delete()
+
+            expect(axios.delete).toHaveBeenCalledWith(expect.any(String), {headers: mockHeaders})
+        })
+
+        it('throws an error on delete failure', async () => {
+            const resource = new ShortcutResource({id: 123})
+            axios.delete.mockRejectedValue(new Error('Error deleting story'))
+
+            await expect(resource.delete()).rejects.toThrow('Error deleting story')
+        })
+    })
+})
