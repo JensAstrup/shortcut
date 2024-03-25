ee4ee3b5e745fb26581d34d105e9c9e191130c1a
Jens Astrup <jensaiden@gmail.com>
2024-03-25 18:27:49 -0400

    Add interface for creating teams

---
+++ src/teams/contracts/create-team-data.ts
@@ -0,0 +1,10 @@
+export default interface CreateTeamData {
+    color?: string
+    colorKey?: string
+    description?: string
+    displayIcon?: string
+    memberIds?: number[]
+    mentionName: string
+    name: string
+    workflowIds?: number[]
+}
