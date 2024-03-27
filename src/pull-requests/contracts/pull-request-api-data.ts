63c0834af8010eb0fd33543f6bd8e9a8c68c4abd
Jens Astrup <jens@wellthy.com>
2024-03-27 09:54:26 -0400

    Add Pull Request type

---
+++ src/pull-requests/contracts/pull-request-api-data.ts
@@ -0,0 +1,26 @@
+export interface PullRequest {
+    branch_id: number
+    branch_name: string
+    build_status: string
+    closed: boolean
+    created_at: string
+    draft: boolean
+    entity_type: string
+    has_overlapping_stories: boolean
+    id: number
+    merged: boolean
+    num_added: number
+    num_commits: number
+    num_modified: number
+    num_removed: number
+    number: number
+    overlapping_stories: number[]
+    repository_id: number
+    review_status: string
+    target_branch_id: number
+    target_branch_name: string
+    title: string
+    updated_at: string
+    url: string
+    vcs_labels: Record<string, unknown>[]
+}
