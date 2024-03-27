3a4642e92e174f3ab8cc8c09e59d006849f981f3
Jens Astrup <jens@wellthy.com>
2024-03-27 09:09:53 -0400

    Add resource classes to export

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 import Client from '@sx/client'
+import Iteration from '@sx/iterations/iteration'
+import Member from '@sx/members/member'
+import {Story} from '@sx/stories/story'
+import Team from '@sx/teams/team'
+import Workflow from '@sx/workflows/workflow'
+
+export default Client
