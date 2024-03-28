a31e1ec7010808eca9bd8ffdc22794e1e4b62c04
Jens Astrup <jens@wellthy.com>
2024-03-28 19:04:17 -0400

    Add epic to docs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import Member from '@sx/members/member'
 import Story from '@sx/stories/story'
 import Team from '@sx/teams/team'
 import Workflow from '@sx/workflows/workflow'
+import Epic from '@sx/epics/epic'
 
 // Services
 import StoriesService from '@sx/stories/stories-service'
@@ -11,7 +12,8 @@ import IterationsService from '@sx/iterations/iterations-service'
 import TeamService from '@sx/teams/team-service'
 import MembersService from '@sx/members/members-service'
 import WorkflowsService from '@sx/workflows/workflows-service'
+import EpicsService from '@sx/epics/epics-service'
 
 export default Client
-export {Client, Iteration, Member, Story, Team, Workflow}
-export {StoriesService, IterationsService, TeamService, MembersService, WorkflowsService}
+export {Client, Iteration, Member, Story, Team, Workflow, Epic}
+export {StoriesService, IterationsService, TeamService, MembersService, WorkflowsService, EpicsService}
