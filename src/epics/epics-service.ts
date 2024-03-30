abba397bcf6148b1352948f01e6bfb33c7d053be
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-03-29 21:45:06 -0400

    44: Add searching capability for epics (#52)

--- src/epics/epics-service.ts
+++ src/epics/epics-service.ts
@@ -1,10 +1,39 @@
 import BaseService from '@sx/base-service'
 import Epic from '@sx/epics/epic'
+import axios from 'axios'
+import EpicApiData from '@sx/epics/contracts/epic-api-data'
+import {convertApiFields} from '@sx/utils/convert-fields'
 
 
 export default class EpicsService extends BaseService<Epic> {
     public baseUrl = 'https://api.app.shortcut.com/api/v3/epics'
     protected factory = (data: object) => new Epic(data)
-    public static epics: Record<number, Epic> = {}
 
+    /**
+     * Search for epics using the [Shortcut Syntax](https://help.shortcut.com/hc/en-us/articles/360000046646-Searching-in-Shortcut-Using-Search-Operators)
+     *
+     * @example
+     * ```typescript
+     * const client = new Client()
+     * const epics = client.epic.search('My epic')
+     * ```
+     *
+     * @throws Error if the HTTP status code is 400 or greater
+     * @param query
+     */
+    public async search(query: string): Promise<Epic[]> {
+        const url = new URL('https://api.app.shortcut.com/api/v3/search/stories')
+        url.search = new URLSearchParams({query: query}).toString()
+
+        const response = await axios.get(url.toString(), {headers: this.headers})
+
+        if (response.status >= 400) {
+            throw new Error('HTTP error ' + response.status)
+
+        }
+
+        const epicData: EpicApiData[] = response.data.data ?? []
+        return epicData.map((epic) => new Epic(convertApiFields(epic)))
+
+    }
 }
---
+++ tests/epics/epics-service.test.js
@@ -0,0 +1,61 @@
+import Epic from '../../src/epics/epic'
+import EpicsService from '../../src/epics/epics-service'
+import axios from 'axios'
+import {convertApiFields} from '../../src/utils/convert-fields'
+
+
+jest.mock('axios')
+jest.mock('../../src/utils/convert-fields', () => {
+    return {
+        convertApiFields: jest.fn().mockImplementation((fields) => fields)
+    }
+
+})
+
+
+describe('Epics service', () => {
+    it('should return an array of epics after searching', async () => {
+        axios.get.mockResolvedValue({
+            data: {
+                data: [{id: 1, name: 'Epic 1', created_at: '2021-01-01T00:00:00Z'},
+                    {id: 2, name: 'Epic 2', created_at: '2021-01-02T00:00:00Z'}]
+            }
+        })
+        // Create expected Epics using the Epic constructor to mimic the actual transformation
+        const expectedEpics = [
+            new Epic({id: 1, name: 'Epic 1', created_at: '2021-01-01T00:00:00Z'}),
+            new Epic({id: 2, name: 'Epic 2', created_at: '2021-01-02T00:00:00Z'})
+        ]
+        const service = new EpicsService({headers: {}})
+        const epics = await service.search('epic')
+
+        // Since we're comparing instances of Epic, either ensure Epic's equality check is appropriate
+        // or compare based on a property that should be equal, like IDs or names.
+        expectedEpics.forEach((expectedEpic, index) => {
+            expect(epics[index]).toEqual(expect.objectContaining({
+                id: expectedEpic.id,
+                name: expectedEpic.name
+            }))
+        })
+
+        expect(convertApiFields).toHaveBeenCalledTimes(2)
+    })
+
+    it('should handle an empty response', () => {
+        axios.get.mockResolvedValue({
+            status: 200,
+            data: []
+        })
+        const service = new EpicsService({headers: {}})
+        expect(service.search('epic')).resolves.toEqual([])
+    })
+
+    it('should throw an error if response is 400 or greater', () => {
+        axios.get.mockResolvedValue({
+            status: 400,
+            data: {error: 'An error occurred'}
+        })
+        const service = new EpicsService({headers: {}})
+        expect(service.search('epic')).rejects.toThrow('HTTP error 400')
+    })
+})
