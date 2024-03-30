194178ee99f7ad516825239c74347fbdf44e330b
Jens Astrup <jensaiden@gmail.com>
2024-03-30 19:13:11 -0400

    Remove StoryInterface from stories index.ts

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -1,5 +1,4 @@
 import Story from '@sx/stories/story'
 import StoriesService from '@sx/stories/stories-service'
-import StoryInterface from '@sx/stories/contracts/story-interface'
 
-export {Story, StoriesService, StoryInterface}
+export {Story, StoriesService}
