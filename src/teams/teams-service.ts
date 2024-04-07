86937565e4797fcbe7b3f5f7aedd6a25a63d8d1b
Jens Astrup <jensaiden@gmail.com>
2024-04-06 23:17:48 -0400

    Lint teams

--- src/teams/teams-service.ts
+++ src/teams/teams-service.ts
@@ -4,25 +4,26 @@ import BaseService, {ServiceOperation} from '@sx/base-service'
 import TeamInterface from '@sx/teams/contracts/team-interface'
 import Team from '@sx/teams/team'
 
+
 export default class TeamsService extends BaseService<Team, TeamInterface> {
-    public baseUrl = 'https://api.app.shortcut.com/api/v3/groups'
-    protected factory = (data: TeamInterface) => new Team(data)
-    public static teams: Record<number, Team> = {}
-    public availableOperations: ServiceOperation[] = ['get', 'list']
+  public baseUrl = 'https://api.app.shortcut.com/api/v3/groups'
+  protected factory = (data: TeamInterface) => new Team(data)
+  public static teams: Record<number, Team> = {}
+  public availableOperations: ServiceOperation[] = ['get', 'list']
 
-    public async enable(): Promise<boolean> {
-        const response = await axios.post(`${this.baseUrl}/enable`, {}, {headers: this.headers})
-        if (response.status >= 400) {
-            throw new Error('HTTP error ' + response.status)
-        }
-        return true
+  public async enable(): Promise<boolean> {
+    const response = await axios.post(`${this.baseUrl}/enable`, {}, {headers: this.headers})
+    if (response.status >= 400) {
+      throw new Error('HTTP error ' + response.status)
     }
+    return true
+  }
 
-    public async disable(): Promise<boolean> {
-        const response = await axios.post(`${this.baseUrl}/disable`, {}, {headers: this.headers})
-        if (response.status >= 400) {
-            throw new Error('HTTP error ' + response.status)
-        }
-        return true
+  public async disable(): Promise<boolean> {
+    const response = await axios.post(`${this.baseUrl}/disable`, {}, {headers: this.headers})
+    if (response.status >= 400) {
+      throw new Error('HTTP error ' + response.status)
     }
+    return true
+  }
 }
