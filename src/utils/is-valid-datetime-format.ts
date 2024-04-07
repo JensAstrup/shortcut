a57bf2c99c00414d448550788946085a63c1070d
Jens Astrup <jensaiden@gmail.com>
2024-04-06 23:16:58 -0400

    Lint utils

--- src/utils/is-valid-datetime-format.ts
+++ src/utils/is-valid-datetime-format.ts
@@ -1,4 +1,4 @@
 export default function isValidDatetimeFormat(text: string): boolean {
-    const pattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
-    return pattern.test(text)
+  const pattern = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z$/
+  return pattern.test(text)
 }
