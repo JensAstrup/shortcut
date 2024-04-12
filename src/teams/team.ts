d32037b61f2cd1422e861e7050667f4361443064
Jens Astrup <JensAstrup@users.noreply.github.com>
2024-04-12 15:31:30 -0400

    142: Add members method to teams and vice versa (#145)

--- src/teams/team.ts
+++ src/teams/team.ts
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 import ShortcutResource, {ResourceOperation} from '@sx/base-resource'
+import Member from '@sx/members/member'
+import MembersService from '@sx/members/members-service'
 import Story from '@sx/stories/story'
 import TeamInterface from '@sx/teams/contracts/team-interface'
 import {convertApiFields} from '@sx/utils/convert-fields'
@@ -10,7 +12,7 @@ import {getHeaders} from '@sx/utils/headers'
 /**
  * @inheritDoc
  */
-export default class Team extends ShortcutResource<TeamInterface> implements TeamInterface {
+class Team extends ShortcutResource<TeamInterface> implements TeamInterface {
   public static baseUrl = 'https://api.app.shortcut.com/api/v3/groups' // Shortcut renamed groups to teams
   public createFields: string[] = ['name', 'mentionName']
   public availableOperations: ResourceOperation[] = ['create', 'update', 'delete']
@@ -19,6 +21,11 @@ export default class Team extends ShortcutResource<TeamInterface> implements Tea
     super()
     Object.assign(this, init)
     this.changedFields = []
+  }
+
+  get members(): Promise<Member[]>{
+    const service = new MembersService({headers: getHeaders()})
+    return service.getMany(this.memberIds)
   }
 
   get stories() {
@@ -49,3 +56,5 @@ export default class Team extends ShortcutResource<TeamInterface> implements Tea
   numStoriesStarted!: number
   workflowIds!: number[]
 }
+
+export default Team
