aa1373bda811382518f8b246c771f3bbc52b54d9
Jens Astrup <jensaiden@gmail.com>
2024-04-15 22:39:13 -0400

    Use m/c js

--- scripts/reformat-imports.js
+++ scripts/reformat-imports.js
@@ -1,10 +1,6 @@
-import fs from 'fs'
-import path from 'path'
-import {fileURLToPath} from 'url'
-
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+// Convert imports to require statements:
+const fs = require('fs')
+const path = require('path')
 
 
 function addJsExtension(file) {
