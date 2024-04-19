b6b424889627eacbe3379a451994d632a2e8de8f
Jens Astrup <jensaiden@gmail.com>
2024-04-19 18:24:15 -0400

    Implement pagination in searching
    This is a breaking change, `search` now returns an object containing the results of the search operation as well as a token to be used to navigate to the next 'page' of results

---
+++ tests/base-service.test.ts
@@ -0,0 +1,152 @@
+import axios from 'axios'
+
+import BaseInterface from '../src/base-interface'
+import ShortcutResource from '../src/base-resource'
+import BaseService, {BaseSearchableService, ServiceOperation} from '../src/base-service'
+import Story from '../src/stories/story'
+
+import mocked = jest.mocked
+
+
+jest.mock('axios')
+
+
+interface MockInterface extends BaseInterface {
+  id: string
+  name: string
+}
+
+class MockResource extends ShortcutResource implements MockInterface {
+  constructor(data: MockInterface) {
+    super(data)
+    Object.assign(this, data)
+  }
+
+  id: string
+  name: string
+
+}
+
+
+class MockService extends BaseService<MockResource, MockInterface> {
+  availableOperations: ServiceOperation[] = ['get', 'list']
+
+  constructor(init: { headers: Record<string, string> }) {
+    super(init)
+    this.baseUrl = 'https://api.mockservice.com/resources'
+    this.factory = (data) => new MockResource(data)
+  }
+}
+
+
+describe('MockService', () => {
+  let mockService: MockService
+  const mockedAxios = mocked(axios)
+
+  beforeEach(() => {
+    mockService = new MockService({headers: {Authorization: 'Bearer token'}})
+    mockService.availableOperations = ['get', 'list', 'search']
+    jest.clearAllMocks()
+  })
+
+  it('should get a resource by ID', async () => {
+    const mockData = {id: '1', name: 'Test Resource'}
+    const mockResponse = {status: 200, data: mockData}
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    mockService.availableOperations = ['get']
+    const resource = await mockService.get('1')
+
+    expect(axios.get).toHaveBeenCalledWith(`${mockService.baseUrl}/1`, {headers: mockService.headers})
+    expect(resource).toBeInstanceOf(MockResource)
+    expect(resource.id).toEqual(mockData.id)
+    expect(resource.name).toEqual(mockData.name)
+  })
+
+  it('should throw an error if get method is not available on resource', async () => {
+    mockService.availableOperations = ['list']
+    expect(mockService.get('1')).rejects.toThrow('Operation not supported')
+  })
+
+  it('should get multiple resources', async () => {
+    const mockData = [{id: '1', name: 'Test Resource 1'}, {id: '2', name: 'Test Resource 2'}]
+    const mockResponse = {status: 200, data: mockData}
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    mockService.availableOperations = ['get']
+    const resources = await mockService.getMany(['1', '2'])
+
+    expect(resources).toBeInstanceOf(Array)
+    expect(resources).toHaveLength(2)
+    expect(resources[0]).toBeInstanceOf(MockResource)
+    expect(resources[1]).toBeInstanceOf(MockResource)
+  })
+
+  it('should list out resources', async () => {
+    const mockData = [{id: '1', name: 'Test Resource 1'}, {id: '2', name: 'Test Resource 2'}]
+    const mockResponse = {status: 200, data: mockData}
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    mockService.availableOperations = ['list']
+    const resources = await mockService.list()
+
+    expect(mockedAxios.get).toHaveBeenCalledWith(mockService.baseUrl, {headers: mockService.headers})
+    expect(resources).toBeInstanceOf(Array)
+    expect(resources).toHaveLength(2)
+    expect(resources[0]).toBeInstanceOf(MockResource)
+    console.log(resources[0])
+    expect(resources[0].id).toEqual(mockData[0].id)
+    expect(resources[0].name).toEqual(mockData[0].name)
+    expect(resources[1]).toBeInstanceOf(MockResource)
+    expect(resources[1].id).toEqual(mockData[1].id)
+    expect(resources[1].name).toEqual(mockData[1].name)
+  })
+
+  it('should throw an error if list method is not available on resource', () => {
+    mockService.availableOperations = ['get']
+    expect(mockService.list()).rejects.toThrow('Operation not supported')
+  })
+})
+
+class MockSearchableService extends BaseSearchableService<MockResource, MockInterface> {
+  availableOperations: ServiceOperation[] = ['get', 'list', 'search']
+
+  constructor(init: { headers: Record<string, string> }) {
+    super(init)
+    this.baseUrl = 'https://api.mockservice.com/resources'
+    this.factory = (data) => new MockResource(data)
+  }
+}
+
+describe('BaseSearchableService', () => {
+  let mockService: MockSearchableService
+  const mockedAxios = mocked(axios)
+
+  beforeEach(() => {
+    mockService = new MockSearchableService({headers: {Authorization: 'Bearer token'}})
+    mockService.availableOperations = ['search']
+    jest.clearAllMocks()
+  })
+
+  it('should return an instance of MockResource', async () => {
+    const mockData = {id: '1', name: 'Test Resource'}
+    const mockResponse = {status: 200, data: {data: [mockData]}}
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    const resources = await mockService.search('test')
+
+    expect(mockedAxios.get).toHaveBeenCalledWith('https://api.app.shortcut.com/api/v3/search/stories?query=test', {headers: mockService.headers})
+    expect(resources.results[0]).toBeInstanceOf(MockResource)
+  })
+
+  it('should search using next page if token is provided', async () => {
+    const mockData = {id: '1', name: 'Test Resource'}
+    const mockResponse = {status: 200, data: {data: [mockData], next: 'https://api.app.shortcut.com/api/v3/search/stories?query=test&page=2'}}
+    mockedAxios.get.mockResolvedValue(mockResponse)
+
+    const resources = await mockService.search('test', 'token')
+
+    expect(mockedAxios.get).toHaveBeenCalledWith('https://api.app.shortcut.com/api/v3/search/stories?page=token&query=test', {headers: mockService.headers})
+    expect(resources.results[0]).toBeInstanceOf(MockResource)
+  })
+})
--- tests/epics/epics-service.test.js
+++ tests/epics/epics-service.test.js
@@ -7,66 +7,66 @@ import {convertApiFields} from '../../src/utils/convert-fields'
 
 jest.mock('axios')
 jest.mock('../../src/utils/convert-fields', () => {
-    return {
-        convertApiFields: jest.fn().mockImplementation((fields) => fields)
-    }
+  return {
+    convertApiFields: jest.fn().mockImplementation((fields) => fields)
+  }
 
 })
 
 describe('Epics service', () => {
-    beforeEach(() => {
-        jest.clearAllMocks()
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  it('should get workflow', () => {
+    axios.get.mockResolvedValue({data: {id: 1, name: 'Workflow 1'}})
+    const service = new EpicsService({headers: {}})
+    expect(service.getWorkflow()).resolves.toEqual({id: 1, name: 'Workflow 1'})
+    expect(axios.get).toHaveBeenCalledWith('https://api.app.shortcut.com/api/v3/epic-workflow', {headers: {}})
+  })
+
+  it('should return an array of epics after searching', async () => {
+    axios.get.mockResolvedValue({
+      data: {
+        data: [{id: 1, name: 'Epic 1', created_at: '2021-01-01T00:00:00Z'},
+          {id: 2, name: 'Epic 2', created_at: '2021-01-02T00:00:00Z'}]
+      }
+    })
+    // Create expected Epics using the Epic constructor to mimic the actual transformation
+    const expectedEpics = [
+      new Epic({id: 1, name: 'Epic 1', created_at: '2021-01-01T00:00:00Z'}),
+      new Epic({id: 2, name: 'Epic 2', created_at: '2021-01-02T00:00:00Z'})
+    ]
+    const service = new EpicsService({headers: {}})
+    const epics = await service.search('epic')
+
+    // Since we're comparing instances of Epic, either ensure Epic's equality check is appropriate
+    // or compare based on a property that should be equal, like IDs or names.
+    expectedEpics.forEach((expectedEpic, index) => {
+      expect(epics.results[index]).toEqual(expect.objectContaining({
+        id: expectedEpic.id,
+        name: expectedEpic.name
+      }))
     })
 
-    it('should get workflow', () => {
-        axios.get.mockResolvedValue({data: {id: 1, name: 'Workflow 1'}})
-        const service = new EpicsService({headers: {}})
-        expect(service.getWorkflow()).resolves.toEqual({id: 1, name: 'Workflow 1'})
-        expect(axios.get).toHaveBeenCalledWith('https://api.app.shortcut.com/api/v3/epic-workflow', {headers: {}})
+    expect(convertApiFields).toHaveBeenCalledTimes(2)
+  })
+
+  it('should handle an empty response', () => {
+    axios.get.mockResolvedValue({
+      status: 200,
+      data: []
     })
+    const service = new EpicsService({headers: {}})
+    expect(service.search('epic')).resolves.toEqual({results: [], next: undefined})
+  })
 
-    it('should return an array of epics after searching', async () => {
-        axios.get.mockResolvedValue({
-            data: {
-                data: [{id: 1, name: 'Epic 1', created_at: '2021-01-01T00:00:00Z'},
-                    {id: 2, name: 'Epic 2', created_at: '2021-01-02T00:00:00Z'}]
-            }
-        })
-        // Create expected Epics using the Epic constructor to mimic the actual transformation
-        const expectedEpics = [
-            new Epic({id: 1, name: 'Epic 1', created_at: '2021-01-01T00:00:00Z'}),
-            new Epic({id: 2, name: 'Epic 2', created_at: '2021-01-02T00:00:00Z'})
-        ]
-        const service = new EpicsService({headers: {}})
-        const epics = await service.search('epic')
-
-        // Since we're comparing instances of Epic, either ensure Epic's equality check is appropriate
-        // or compare based on a property that should be equal, like IDs or names.
-        expectedEpics.forEach((expectedEpic, index) => {
-            expect(epics[index]).toEqual(expect.objectContaining({
-                id: expectedEpic.id,
-                name: expectedEpic.name
-            }))
-        })
-
-        expect(convertApiFields).toHaveBeenCalledTimes(2)
+  it('should throw an error if response is 400 or greater', () => {
+    axios.get.mockResolvedValue({
+      status: 400,
+      data: {error: 'An error occurred'}
     })
-
-    it('should handle an empty response', () => {
-        axios.get.mockResolvedValue({
-            status: 200,
-            data: []
-        })
-        const service = new EpicsService({headers: {}})
-        expect(service.search('epic')).resolves.toEqual([])
-    })
-
-    it('should throw an error if response is 400 or greater', () => {
-        axios.get.mockResolvedValue({
-            status: 400,
-            data: {error: 'An error occurred'}
-        })
-        const service = new EpicsService({headers: {}})
-        expect(service.search('epic')).rejects.toThrow('HTTP error 400')
-    })
+    const service = new EpicsService({headers: {}})
+    expect(service.search('epic')).rejects.toThrow('HTTP error 400')
+  })
 })
