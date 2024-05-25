2c755c994761e9f5d286f37da2a2c7067b4104a6
Jens Astrup <jensaiden@gmail.com>
2024-05-25 00:52:20 -0400

    Update custom eslint base config, only run linter on changed files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,8 @@ module.exports = {
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
     "ecmaVersion": "latest",
-    "sourceType": "module"
+    "sourceType": "module",
+    "project": "./tsconfig.json"
   },
   "plugins": [
     "@typescript-eslint",
