85890fe8e95aec46fbd3865e9cdbc975f31ea105
Jens Astrup <jensaiden@gmail.com>
2024-06-25 00:07:36 -0400

    Save shortcut token to environment

--- src/client.ts
+++ src/client.ts
@@ -15,12 +15,13 @@ import UploadedFilesService from '@sx/uploaded-files/uploaded-files-service'
 import WorkflowService from '@sx/workflows/workflows-service'
 
 
-export type ShortcutHeaders = { 'Content-Type': string; 'Shortcut-Token': string }
+export type ShortcutHeaders = { 'Content-Type': string, 'Shortcut-Token': string }
 
 
 export default class Client {
   get headers(): ShortcutHeaders {
     if (this.shortcutApiKey) {
+      process.env.SHORTCUT_API_KEY = this.shortcutApiKey
       return {
         'Content-Type': 'application/json',
         'Shortcut-Token': this.shortcutApiKey
@@ -52,19 +53,18 @@ export default class Client {
 
   constructor(shortcutApiKey?: string) {
     if (shortcutApiKey) this.shortcutApiKey = shortcutApiKey
-    this.stories = new StoriesService({headers: this.headers})
-    this.workflows = new WorkflowService({headers: this.headers})
-    this.iterations = new IterationsService({headers: this.headers})
-    this.teams = new TeamsService({headers: this.headers})
-    this.members = new MembersService({headers: this.headers})
-    this.epics = new EpicsService({headers: this.headers})
-    this.objectives = new ObjectivesService({headers: this.headers})
-    this.keyResults = new KeyResultsService({headers: this.headers})
-    this.labels = new LabelsService({headers: this.headers})
-    this.customFields = new CustomFieldsService({headers: this.headers})
-    this.linkedFiles = new LinkedFilesService({headers: this.headers})
-    this.uploadedFiles = new UploadedFilesService({headers: this.headers})
-    this.repositories = new RepositoriesService({headers: this.headers})
+    this.stories = new StoriesService({ headers: this.headers })
+    this.workflows = new WorkflowService({ headers: this.headers })
+    this.iterations = new IterationsService({ headers: this.headers })
+    this.teams = new TeamsService({ headers: this.headers })
+    this.members = new MembersService({ headers: this.headers })
+    this.epics = new EpicsService({ headers: this.headers })
+    this.objectives = new ObjectivesService({ headers: this.headers })
+    this.keyResults = new KeyResultsService({ headers: this.headers })
+    this.labels = new LabelsService({ headers: this.headers })
+    this.customFields = new CustomFieldsService({ headers: this.headers })
+    this.linkedFiles = new LinkedFilesService({ headers: this.headers })
+    this.uploadedFiles = new UploadedFilesService({ headers: this.headers })
+    this.repositories = new RepositoriesService({ headers: this.headers })
   }
-
 }
